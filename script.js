24fb409d998684101215e3ed8595d310039d667f
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-02-24 09:25:23 +0100

    ajouter la fonction operate() contenant switch pour appliquer l'operation convenable avec l'operateur en parametre

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ function divise(nb1,nb2) {
     }
     return nb1/nb2;
 }
+// console.log(add(2,1));
+function operate(operateur,n1,n2) {
+    switch (operateur) {
+        case "+":
+            result=add(n1,n2);
+        break;
+        case "-":
+            result=soustract(n1,n2);
+        break;
+        case "*":
+            result=multiple(n1,n2);
+        break;
+        case "/":
+            result=division(n1,n2);
+        break;
+        default:
+            result=0;
+            break;
+    }
+    return result;
+}
