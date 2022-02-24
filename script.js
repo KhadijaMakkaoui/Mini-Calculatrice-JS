13ac0227001325c53549d29f6e71e99a08abd724
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-02-24 09:00:20 +0100

    Ajouter les fct des operation addition, soustraction, multiplication et division

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+function add(nb1,nb2) {
+    return nb1+nb2;
+}
+function soustract(nb1,nb2) {
+    return nb1-nb2;
+}
+function multiple(nb1,nb2) {
+    return nb1*nb2;
+}
+function divise(nb1,nb2) {
+    if(nb2==0){
+        return "Désoler...la division sur 0 zéro ne peut pas être éffectué ";
+    }
+    return nb1/nb2;
+}
