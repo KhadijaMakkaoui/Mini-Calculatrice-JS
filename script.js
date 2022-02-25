1a5e958595bd29a786ace9641009a869f679b328
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-02-25 10:57:29 +0100

    Clear and backspace Ajouter une fonction supprimer permer de vider le champ d'affichage Ajouter une fonction backspace permet de supprimer le dernier caractère taper dans la zone d'affichage

--- script.js
+++ script.js
@@ -1,36 +1,50 @@
-function add(nb1,nb2) {
-    return nb1+nb2;
+function add(nb1, nb2) {
+    return nb1 + nb2;
 }
-function soustract(nb1,nb2) {
-    return nb1-nb2;
+
+function soustract(nb1, nb2) {
+    return nb1 - nb2;
 }
-function multiple(nb1,nb2) {
-    return nb1*nb2;
+
+function multiple(nb1, nb2) {
+    return nb1 * nb2;
 }
-function divise(nb1,nb2) {
-    if(nb2==0){
+
+function divise(nb1, nb2) {
+    if (nb2 == 0) {
         return "Désoler...la division sur 0 zéro ne peut pas être éffectué ";
     }
-    return nb1/nb2;
+    return nb1 / nb2;
 }
 // console.log(add(2,1));
-function operate(operateur,n1,n2) {
+function operate(operateur, n1, n2) {
     switch (operateur) {
         case "+":
-            result=add(n1,n2);
-        break;
+            result = add(n1, n2);
+            break;
         case "-":
-            result=soustract(n1,n2);
-        break;
+            result = soustract(n1, n2);
+            break;
         case "*":
-            result=multiple(n1,n2);
-        break;
+            result = multiple(n1, n2);
+            break;
         case "/":
-            result=division(n1,n2);
-        break;
+            result = division(n1, n2);
+            break;
         default:
-            result=0;
+            result = 0;
             break;
     }
     return result;
+}
+
+//Supprimer tous
+function Supprimer() {
+    isfloat = false;
+    // alert("Please select");
+    output.value = " ";
+}
+
+function backspace() {
+    output.value = output.value.slice(0, -1);
 }
