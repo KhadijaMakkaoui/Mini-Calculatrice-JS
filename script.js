73a53eab7f5b9584dc3234295505be4fe8aec955
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-03-07 16:16:17 +0100

    Créer la fonction OperationChoix permet d'afficher le nombre taper aprés clicker sur un operateur ou afficher le resultat si il existe déja un nombre premier

--- script.js
+++ script.js
@@ -16,11 +16,19 @@ function divise(nb1, nb2) {
     }
     return nb1 / nb2;
 }
+isfloat = false;
+current = document.getElementById("input_Affichage");
+previous = document.getElementById("result");
+
 // console.log(add(2,1));
-function operate(operateur, n1, n2) {
+function operate(operateur, p, c) {
+    let n1 = parseFloat(p);
+    let n2 = parseFloat(c);
+
     switch (operateur) {
         case "+":
             result = add(n1, n2);
+
             break;
         case "-":
             result = soustract(n1, n2);
@@ -35,16 +43,55 @@ function operate(operateur, n1, n2) {
             result = 0;
             break;
     }
-    return result;
+    return parseFloat(result);
 }
 
+function OperationChoix(operateur) {
+    valPrev = previous.value;
+    // valPrev = previous.value;
+
+    if (!previous.value) {
+
+        previous.value = current.value + operateur;
+    } else {
+        previous.value = operate(operateur, valResult, current) + operateur;
+    }
+    current.value = " ";
+}
+// previous.value = operate(operateur, previous, current);
+
+
+
+//Affichage 
+function afficher(clicked_value) {
+
+    document.getElementById("input_Affichage").value += clicked_value;
+    return clicked_value;
+    // operate()
+    // if (clicked_value == ".") {
+    //     // if (output.value == " ") {
+    //     //     output.value += 0;
+    //     // }
+    //     output.value += clicked_value;
+    //     isfloat = true;
+    // } else {
+    //     if (isfloat) {
+    //         document.getElementById("point").disabled = true;
+    //         output.value += clicked_value;
+    //     } else {
+    //         // document.getElementById("point").disabled=false;
+    //         output.value += clicked_value;
+    //     }
+    // }
+}
 //Supprimer tous
-function Supprimer() {
-    isfloat = false;
+function ClearAll() {
+    // isfloat = false;
     // alert("Please select");
-    output.value = " ";
+    current.value = " ";
+    previous.value = " ";
 }
 
 function backspace() {
-    output.value = output.value.slice(0, -1);
+    current.value = current.value.slice(0, -1);
 }
