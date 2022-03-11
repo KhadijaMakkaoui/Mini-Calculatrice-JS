dd31313c7147b8b5178f25a15095609e628e822a
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-03-11 11:12:54 +0100

    Ajouter un tableau qui stocke les operateur appliquer l'operateur precedent sur la fonction operate

--- script.js
+++ script.js
@@ -17,28 +17,34 @@ function divise(nb1, nb2) {
     }
     return nb1 / nb2;
 }
-let result = 0;
+let result = 0,
+    res2 = 0;
 let current = document.getElementById("input_Affichage");
 let previous = document.getElementById("result");
 let pointbtn = document.getElementById("point");
-let disCurrent, disPrevious;
-let first_op, second_op, i = 0,
-    j = 1;
-let nb_entrer = 0;
+let i = 0,
+    countOp = 0;
 let operateur;
 let equals;
+let n1 = 0,
+    n2 = 0;
 
 function operate(op) {
+
     n2 = parseFloat(current.value);
+    n1 = parseFloat(res2);
+    // if (countOp == 0) {
+    //     n1 = parseFloat(previous.value);
 
-    if (previous.value != "")
-        n1 = parseFloat(previous.value);
-    else
-        n1 = parseFloat(result);
+    //     countOp++
+    //     return
+    // } else
 
-    if (isNaN(n1) || isNaN(n2))
-        return
 
+    // countOp++
+    // if (isNaN(n1) || isNaN(n2) || n1 || isNaN(n2))
+    //     return
+    // if (n1 != 0 || n2 != 0)
     switch (op) {
         case "+":
             result = add(n1, n2);
@@ -53,37 +59,44 @@ function operate(op) {
             result = divise(n1, n2);
             break;
     }
-
 }
 
-function Negative() {
-    current.value = current.value * (-1);
-}
+//tableau des operateurs de l'operation
+let arOperator = new Array();
 
 function OperationChoix(op) {
-
+    //sortir de la fonction si l'utilisateur n'a rien entrer
     if (current.value == "") {
         return;
     }
-    if (previous.value != "")
-        operate(op)
-
+    //vider les variable aprés le clique sur egual
     if (equals) {
         previous.value = "";
+        arOperator = [];
+        result = 0;
+        res2 = 0;
     }
-    if (op != "=") {
-        previous.value += current.value + "" + op;
-        equals = false;
-    } else {
-        previous.value += current.value + "" + op + result;
-        equals = true;
-    }
-    current.value = "";
 
+    previous.value += current.value + "" + op;
+    arOperator[i] = op;
+    operate(arOperator[i - 1])
+
+    if (res2 == 0) {
+        res2 = current.value;
+    } else
+        res2 = result;
+    document.querySelector("h4").innerHTML = result;
+    i++;
+    //tester si l'utilisateur a cliquer sur egal
+    if (op != "=")
+        equals = false;
+    else
+        equals = true;
+
+    current.value = "";
+    //reactiver le boutton de valeur point
     pointbtn.removeAttribute("disabled");
 }
-
-
 
 //Affichage 
 function afficher(clicked_value) {
@@ -94,12 +107,18 @@ function afficher(clicked_value) {
 
     current.value += clicked_value;
 }
+//multiplier par -1 pour rendre un nombre negatif
+function Negative() {
+    current.value = current.value * (-1);
+}
 //Supprimer tous
 function ClearAll() {
     // isfloat = false;
     current.value = "";
     previous.value = "";
     result = 0;
+    arOperator = [];
+    document.querySelector("h4").innerHTML = "";
 }
 
 function backspace() {
