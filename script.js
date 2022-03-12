47bc8c6bf48d6675d79b50570f5c971fc29a1108
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-03-12 21:05:32 +0100

    Traiter le cas de vergule sans valeur precedante ajouter un zero si l'utilisateur clicke sur point sans clicker sur une valeur avant

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let result = 0,
+    res2 = 0;
+
+const current = document.getElementById("input_Affichage");
+const previous = document.getElementById("result");
+const pointbtn = document.getElementById("point");
+const btnSigne = document.getElementById("signe");
+const btnBackSp = document.getElementById("backspace");
+const btnCA = document.getElementById("clearall");
+let i = 0,
+    countOp = 0;
+let operateur;
+let equals;
+let n1 = 0,
+    n2 = 0;
+//tableau des operateurs de l'operation
+let arOperator = new Array();
+
 function add(nb1, nb2) {
     return nb1 + nb2;
 }
@@ -13,21 +31,10 @@ function multiple(nb1, nb2) {
 function divise(nb1, nb2) {
     if (nb2 == 0) {
         alert("Désoler...la division sur 0 zéro ne peut pas être éffectué ");
-        return "";
+        return nb1;
     }
     return nb1 / nb2;
 }
-let result = 0,
-    res2 = 0;
-let current = document.getElementById("input_Affichage");
-let previous = document.getElementById("result");
-let pointbtn = document.getElementById("point");
-let i = 0,
-    countOp = 0;
-let operateur;
-let equals;
-let n1 = 0,
-    n2 = 0;
 
 function operate(op) {
 
@@ -39,8 +46,6 @@ function operate(op) {
     //     countOp++
     //     return
     // } else
-
-
     // countOp++
     // if (isNaN(n1) || isNaN(n2) || n1 || isNaN(n2))
     //     return
@@ -61,9 +66,6 @@ function operate(op) {
     }
 }
 
-//tableau des operateurs de l'operation
-let arOperator = new Array();
-
 function OperationChoix(op) {
     //sortir de la fonction si l'utilisateur n'a rien entrer
     if (current.value == "") {
@@ -76,9 +78,11 @@ function OperationChoix(op) {
         result = 0;
         res2 = 0;
     }
-
+    //Afficher les operations entrer par l'utilisateur
     previous.value += current.value + "" + op;
+    //stocker l'operateur dans le tableau
     arOperator[i] = op;
+    //Effectuer l'operation à l'aide du premier operateur entrer
     operate(arOperator[i - 1])
 
     if (res2 == 0) {
@@ -98,24 +102,13 @@ function OperationChoix(op) {
     pointbtn.removeAttribute("disabled");
 }
 
-//Affichage 
-function afficher(clicked_value) {
-    if (current.value.includes("."))
-        pointbtn.setAttribute("disabled", "true");
-    else
-        pointbtn.removeAttribute("disabled");
-
-    current.value += clicked_value;
-}
-const btnSigne = document.getElementById("signe");
 //multiplier par -1 pour rendre un nombre negatif
 btnSigne.addEventListener("click", function Negative() {
-        current.value = current.value * (-1);
-    })
-    //Supprimer tous
-const btnCA = document.getElementById("clearall");
+    current.value = current.value * (-1);
+})
+
+//Supprimer tous
 btnCA.addEventListener("click", function ClearAll() {
-    // isfloat = false;
     current.value = "";
     previous.value = "";
     result = 0;
@@ -123,13 +116,28 @@ btnCA.addEventListener("click", function ClearAll() {
     document.querySelector("h4").innerHTML = "";
 })
 
-const btnBackSp = document.getElementById("backspace");
+//Backspace
 btnBackSp.addEventListener("click", function backspace() {
     current.value = current.value.slice(0, -1);
 })
-
-
 const numbers = document.querySelectorAll(".numbers")
 numbers.forEach(button => {
     button.addEventListener("click", (e) => afficher(button.value))
 });
+
+function afficher(clicked_value) {
+
+    if (current.value.includes("."))
+        pointbtn.setAttribute("disabled", "true");
+    else
+        pointbtn.removeAttribute("disabled");
+    if (current.value == "" && clicked_value == ".")
+        current.value += "0" + clicked_value;
+    else
+        current.value += clicked_value;
+}
+const operateSignes = document.querySelectorAll(".operateSigne");
+operateSignes.forEach(button => {
+    button.addEventListener("click", (e) => OperationChoix(button.value))
+
+})
